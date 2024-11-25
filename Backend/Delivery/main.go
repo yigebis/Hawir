11d8dbf7afa19cf6fcceac154996f73053130813
Yigerem <yigerem4@gmail.com>
2024-11-25 19:07:19 +0300

    simple registration with email verification done;some validations

--- Backend/Delivery/main.go
+++ Backend/Delivery/main.go
@@ -1,1 +1,72 @@
 package main
+
+import (
+	"Hawir/Delivery/Controller"
+	"Hawir/Delivery/Router"
+	"Hawir/Error"
+	"Hawir/Infrastructure"
+	"Hawir/Repository"
+	"Hawir/UseCase"
+
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("error loading .env file")
+	}
+
+	// setting up the usecase
+
+	//user usecase
+	username := os.Getenv("MONGO_USERNAME")
+	password := os.Getenv("MONGO_PASSWORD")
+	uri := "mongodb+srv://" + username + ":" + password + "@cluster0.isgee.mongodb.net/"
+
+	fmt.Print(uri)
+
+	clientOptions := options.Client().ApplyURI(uri)
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(context.TODO(), nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to db!")
+	user_collection := client.Database("Hawir").Collection("users")
+
+	user_context := context.TODO()
+
+	ur := Repository.NewUserRepository(user_context, user_collection)
+
+	jwtSecret := os.Getenv("JWT_SECRET")
+	ps := Infrastructure.NewPasswordService()
+	ts := Infrastructure.NewTokenService(jwtSecret)
+	ms := Infrastructure.NewMailService(os.Getenv("SENDER_EMAIL"), os.Getenv("EMAIL_PASSWORD"), os.Getenv("FROM"))
+	es := Error.NewErrorService()
+
+	ex := os.Getenv("EMAIL_EXPIRY")
+	tx := os.Getenv("TOKEN_EXPIRY")
+
+	uuc := UseCase.NewUserUseCase(ur, ps, ts, ms, es, ex, tx)
+
+	// setting up the controllers
+	user_controller := Controller.NewUserController(uuc, ts)
+
+	// setting up the router
+	router := Router.NewRouter(user_controller)
+	router.Run()
+}
--- Backend/Domain/user.go
+++ Backend/Domain/user.go
@@ -1,1 +1,37 @@
 package Domain
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type User struct {
+	ID                primitive.ObjectID `json:"id" bson:"_id,omitempty"`
+	FirstName         string             `json:"first_name" bson:"first_name" validate:"required,min=1,max=50"`
+	LastName          string             `json:"last_name" bson:"last_name" validate:"required,min=1,max=50"`
+	LoginPreference   string             `json:"login_preference" bson:"login_preference" validate:"required"`
+	Email             string             `json:"email" bson:"email" validate:"email"`
+	PhoneNumber       string             `json:"phone_number" bson:"phone_number"`
+	Password          string             `json:"password" bson:"password" validate:"required,min=8"`
+	ProfilePhoto      string             `json:"profile_photo" bson:"profile_photo"`
+	RegistrationDate  time.Time          `json:"registration_date" bson:"registration_date"`
+	Verified          bool               `json:"verified" bson:"verified"`
+	FavouriteAgencies []string           `json:"favourite_agencies" bson:"favourite_agencies"`
+}
+
+type EmailCredential struct {
+	Email    string `json:"email" bson:"email"`
+	Password string `json:"password" bson:"password"`
+}
+
+type PhoneCredential struct {
+	PhoneNumber string `json:"phone_number" bson:"phone_number"`
+	Password    string `json:"password" bson:"password"`
+}
+
+type ChangeCredential struct {
+	Email       string `json:"email" bson:"email"`
+	OldPassword string `json:"old_password" bson:"old_password"`
+	NewPassword string `json:"new_password" bson:"new_password"`
+}
