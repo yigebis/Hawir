e4e2a636ce0a8f24cf0a16ba50711eba677053dc
Yigerem <yigerem4@gmail.com>
2024-11-27 14:46:22 +0300

    oauth2 with google done;login completed

---
+++ Backend/Infrastructure/oauth2.go
@@ -0,0 +1,29 @@
+package Infrastructure
+
+import (
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+)
+
+type OAuth struct {
+	OAuthState  string
+	OAuthConfig *oauth2.Config
+}
+
+func NewOAuth(oauthState string, oauthClientID string, oauthClientSecret string) *OAuth {
+
+	return &OAuth{
+		OAuthState: oauthState,
+		OAuthConfig: &oauth2.Config{
+			ClientID:     oauthClientID,                         // Replace with your Client ID
+			ClientSecret: oauthClientSecret,                     // Replace with your Client Secret
+			RedirectURL:  "http://localhost:8080/auth/callback", // Replace with your redirect URI
+			Scopes:       []string{"https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email"},
+			Endpoint:     google.Endpoint,
+		},
+	}
+}
+
+func (o *OAuth) GetOAuthURL() string {
+	return o.OAuthConfig.AuthCodeURL(o.OAuthState, oauth2.AccessTypeOffline)
+}
