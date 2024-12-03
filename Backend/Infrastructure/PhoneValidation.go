52677402140f9fcdd9b477d670e11e0e1379aa74
TedXpro <johannes.woldeyes@gmail.com>
2024-12-03 14:53:32 +0300

    check if phonenumber starts with 7  or 9

--- Backend/Infrastructure/PhoneValidation.go
+++ Backend/Infrastructure/PhoneValidation.go
@@ -9,7 +9,7 @@ func NewValidationService() *ValidationService {
 }
 
 func (vs *ValidationService) PhoneValidation(phoneNumber string) bool {
-	if len(phoneNumber) < 10 { // checks if the length is less than 10
+	if len(phoneNumber) != 9 || (phoneNumber[0] != '7' && phoneNumber[0] != '9'){ // checks if the length is not equall to 9 or starts with either 7 or 9
 		return false
 	}
 
