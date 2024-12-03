e10a7365f8ab8a38b3327bd712fa951db6c8ea90
yigebis <122978889+yigebis@users.noreply.github.com>
2024-12-03 15:27:17 +0300

    Update router.go

--- Backend/Delivery/Router/router.go
+++ Backend/Delivery/Router/router.go
@@ -3,6 +3,7 @@ package Router
 import (
 	"Hawir/Delivery/Controller"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -20,12 +21,23 @@ func NewRouter(uc *Controller.UserController) *Router {
 func (r *Router) Run() {
 	router := gin.Default()
 
-	router.POST("/register", r.UserController.Register)
-	router.POST("/login/email", r.UserController.LoginByEmail)
-	router.POST("/login/phone_number", r.UserController.LoginByPhoneNumber)
+	router.POST("/api/register", r.UserController.Register)
+	router.POST("/api/login/email", r.UserController.LoginByEmail)
+	router.POST("/api/login/phone_number", r.UserController.LoginByPhoneNumber)
 	router.GET("/verify", r.UserController.VerifyEmail)
+	router.GET("/email/reject", r.UserController.RejectEmail)
 	router.GET("/auth/with/google", r.UserController.LoginWithGoogle) //redirects to google login page
 	router.GET("/auth/callback", r.UserController.GoogleCallback)     //user already agreed to give his info so google will redirect to this endpoint
 
+	config := cors.Config{
+		AllowOrigins:     []string{"http://localhost:5173"},                   // Frontend URL
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}, // HTTP methods to allow
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"}, // Headers to allow
+		ExposeHeaders:    []string{"Content-Length"},                          // Headers to expose to frontend
+		AllowCredentials: true,                                                // Allow cookies or authentication headers
+	}
+
+	// Apply CORS middleware
+	router.Use(cors.New(config))
 	router.Run()
 }
