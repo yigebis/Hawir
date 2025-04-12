fe2889e9af4779dd3ca097d74f59ee616f6d7299
yigebis <122978889+yigebis@users.noreply.github.com>
2025-04-12 16:57:25 +0300

    Update main.go for deploy

--- Backend/Delivery/main.go
+++ Backend/Delivery/main.go
@@ -22,9 +22,9 @@ import (
 
 func main() {
 	//comment it for production
-	if err := godotenv.Load(); err != nil {
-		log.Fatal("error loading .env file")
-	}
+	// if err := godotenv.Load(); err != nil {
+	// 	log.Fatal("error loading .env file")
+	// }
 
 	// domain name of the website
 	websiteDomainName := os.Getenv("WEBSITE_DOMAIN_NAME")
