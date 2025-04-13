7c3b7dea1305b7ea9d969f0eb919d0ee7bb6ca77
TedXpro <johannes.woldeyes@gmail.com>
2025-04-13 08:14:00 +0300

    modify the edit travel to include actual and planned start time

--- Backend/UseCase/travelUseCase.go
+++ Backend/UseCase/travelUseCase.go
@@ -63,6 +63,8 @@ func (tuc *TravelUseCase) CreateTravel(travel *Domain.Travel) (int, error) {
 	travel.PostTime = time.Now()
 	travel.LastModTime = travel.PostTime
 	travel.Status = "upcoming"
+	travel.ActualStartTime = travel.PlannedStartTime
+	travel.ActualArrivalTime = travel.EstArrivalTime
 
 	travelID, err := tuc.TravelRepo.CreateTravel(travel)
 	if err != nil {
@@ -92,6 +94,9 @@ func (tuc *TravelUseCase) EditTravel(travel *Domain.Travel) (int, error) {
 	if err != nil {
 		return code, err
 	}
+
+	travel.ActualStartTime = travel.PlannedStartTime
+	travel.ActualArrivalTime = travel.EstArrivalTime
 
 	err = tuc.TravelRepo.EditTravel(travel)
 	if err != nil {
