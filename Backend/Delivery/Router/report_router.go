99d4f0be1ba1481644931bdd93ec3bfb3d7ea0f4
Yigerem <yigerem4@gmail.com>
2025-06-11 00:36:43 +0300

    report generation

--- Backend/Delivery/Router/report_router.go
+++ Backend/Delivery/Router/report_router.go
@@ -19,4 +19,9 @@ func NewReportRouter(reportController Controller.ReportController) *ReportRouter
 
 func (rr *ReportRouter) Run(router *gin.Engine, jwt_string string) {
 	router.GET("/agency/reports/book_heatmap", Infrastructure.AgencyMiddleWare(jwt_string), rr.ReportController.GetBookHeatMap)
+	router.GET("/agency/reports/active_travelers_count", Infrastructure.AgencyMiddleWare(jwt_string), rr.ReportController.GetActiveTravelersCount)
+	router.GET("/agency/reports/top_five_destinations", Infrastructure.AgencyMiddleWare(jwt_string), rr.ReportController.GetTopFiveDestinations)
+	router.GET("/agency/reports/trip_heatmap", Infrastructure.AgencyMiddleWare(jwt_string), rr.ReportController.GetTripHeatMap)
+	router.GET("/agency/reports/revenue", Infrastructure.AgencyMiddleWare(jwt_string), rr.ReportController.GetRevenueReport)
+	router.GET("/agency/reports/new_customers", Infrastructure.AgencyMiddleWare(jwt_string), rr.ReportController.GetNewCustomersReport)
 }
--- Backend/UseCase/reportInterface.go
+++ Backend/UseCase/reportInterface.go
@@ -1,11 +1,23 @@
 package UseCase
 
+import "Hawir/Domain"
+
 // "Hawir/Domain"
 
 type IReportUseCase interface {
 	GetBookHeatMap(agencyID string) (*[]int, int, error)
+	GetActiveTravelersCount(agencyID string) (*Domain.ActiveTravelersCount, int, error)
+	GetTopFiveDestinations(agencyID string) (*[]Domain.Destination, int, error)
+	GetTripHeatMap(agencyID string) (*[]int, int, error)
+	GetRevenueReport(agencyID string) (*[]int, int, error)
+	GetNewCustomersReport(agencyID string) (*[]int, int, error)
 }
 
 type IReportRepository interface {
 	GetBookHeatMap(agencyID string) (*map[string]int, error)
+	GetActiveTravelersCount(agencyID string) (*Domain.ActiveTravelersCount, error)
+	GetTopFiveDestinations(agencyID string) (*[]Domain.Destination, error)
+	GetTripHeatMap(agencyID string) (*map[string]int, error)
+	GetRevenueReport(agencyID string) (*map[string]int, error)
+	GetNewCustomersReport(agencyID string) (*map[string]int, error)
 }
